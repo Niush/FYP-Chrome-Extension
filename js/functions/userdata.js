175c049845253ecf4ded089c3c90a19116a50cfc
Niush <niushsitaula@gmail.com>
2019-03-13 16:30:36 +0545

    More Getter, Setter + SKELETON FOR SYNC AND USER VALIDATE

--- js/functions/userdata.js
+++ js/functions/userdata.js
@@ -4,32 +4,60 @@ class User{
 	constructor(){
 		chrome.storage.local.get(['user_data'], function(result) {
 		  if(result.user_data == undefined || result.user_data == ''){
-			this.resetLocal();
+			new User().resetLocal();
 		  }else{
 			data = JSON.parse(result.user_data);
 		  }
 		});
-		
-		/* if(!localStorage.hasOwnProperty('user_data')){
-			this.resetLocal();
-		}else{
-			data = JSON.parse(localStorage.getItem("user_data"));
-		} */
+	}
+	
+	getUTC(){
+		return new Date().getTime();
 	}
 	
 	// TODO: Verifiy User Id and Passphrase from server 
 	checkUserAuth(){
-		if(internet_status()){
+		if(internetStatus()){
 			// Check if user id good
+			return true;
+			//else{return 'User is Invalid. Please Logout and Login again.\nWeird stuff.';}
 		}else{
 			return 'Internet Connection Not Found';
 		}
 	}
 	
-	syncNow(){
-		if(checkUserAuth()){
+	syncNow(sender='app'){
+		let x;
+		if((x = this.checkUserAuth()) == true){
+			//TODO: ALL SYNCING TO SERVER
+        	//Start Syncing Data Here...
+			// 1. SYNC NOTE
+			for(let i = 0 ; i < data.notes.length ; i++){
+				if(data.notes[i].synced == 0){
+					// AJAX SYNC THIS data.notes[i] object
+					
+					//data.notes[i].synced = 1;
+				}
+			}
+			data.last_sync = this.getUTC();
+			console.log(data.last_sync);
 			
-		}
+			// AJAX SYNC FOCUS
+			// JSON.stringify(data.focus)
+			
+			// AJAX SYNC Disable App
+			// JSON.stringify(data.focus)
+			
+			this.updateLocal();
+			
+        }else{
+			//Show the CheckUserAuth Error Message//
+			if(sender == 'user'){
+				alert("Minimal Productivity Extension: " +x);
+			}else{
+				console.log(x);
+			}
+        }
 	}
 	
 	// Updates the chrome.storage.local to data json
@@ -75,4 +103,60 @@ class User{
 		data['passphrase'] = passphrase;
 		this.updateLocal();
 	}
+	
+	/* Last Sync */
+	get last_sync(){
+		return data['last_sync'];
+	}
+	set last_sync(time){
+		data['last_sync'] = time;
+		this.updateLocal();
+	}
+	get last_sync_difference(){
+		return Math.abs(parseInt(this.getUTC()) - parseInt(this.last_sync)) / 36e5;
+	}
+
+	/* focus_modified_at */
+	get focus_modified_at(){
+		return data['focus_modified_at'];
+	}
+	set focus_modified_at(time){
+		data['focus_modified_at'] = time;
+		this.updateLocal();
+	}
+
+	/* disable_modified_at */
+	get disable_modified_at(){
+		return data['disable_modified_at'];
+	}
+	set disable_modified_at(time){
+		data['disable_modified_at'] = time;
+		this.updateLocal();
+	}
+	
+	/* LOCATION */
+	get location(){
+		return data['location'];
+	}
+	set location(location){
+		data['location'] = location;
+		this.updateLocal();
+	}
+
+	/* DIM TIME */
+	get dim_time(){
+		return data['dim_time'];
+	}
+	set dim_time(from,to){
+		data['dim_time'] = [from,to];
+		this.updateLocal();
+	}
+	
+	/* NOTES */
+	
+	/* FOCUS */
+	
+	/* DISABLE APP - Also, a method to check if url is in disabled app list */
+	
+	/* TO-DO */
 }

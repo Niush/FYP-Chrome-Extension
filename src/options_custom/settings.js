6871f9a8114dfbd757fa5dac5a019d51423658ff
Niush <niushsitaula@gmail.com>
2019-03-23 21:11:10 +0545

    Settings Page Edited with Welcome message and detect firstrun with (?show=welcome) and some checks + 1em font body

--- src/options_custom/settings.js
+++ src/options_custom/settings.js
@@ -1,3 +1,35 @@
 document.addEventListener('DOMContentLoaded', function() {
-	var instance = M.Tabs.init(document.getElementsByClassName('tabs')[0], {});
+	var tabs = M.Tabs.init(document.getElementsByClassName('tabs')[0], {});
+	var welcomeModal = M.Modal.init(document.getElementById('welcomeModal'), {dismissible: false, inDuration: 0, opacity: 0.9,});
+	checkFirstRun(welcomeModal);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	// AFTER ALL LOADED - REMOVE LOADING SCREEN//
+	setTimeout(function(){
+		document.getElementById('loading').className += ' hidden';
+		setTimeout(function(){
+			document.getElementById('loading').remove();
+		}, 1000);
+	}, 500);
 });
+
+function checkFirstRun(modal){
+	if(getRequest('show') == 'welcome'){
+		modal.open();
+	}
+}
+
+function getRequest(name){
+   if(name=(new RegExp('[?&]'+encodeURIComponent(name)+'=([^&]*)')).exec(location.search))
+      return decodeURIComponent(name[1]);
+}
